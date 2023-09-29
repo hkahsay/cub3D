e9df09ac740a5a8efcd1802c656354e3113d2264
hkahsay <90785664+hkahsay@users.noreply.github.com>
2023-09-29 12:06:24 +0200

    add movment to keypress

--- src/parsing/ft_free.c
+++ src/parsing/ft_free.c
@@ -27,14 +27,3 @@ void    free_strs_array(char **strs)
     }
     free(strs);
 }
-
-void free_line_list(t_lineNode *head)
-{
-    while (head != NULL)
-    {
-        t_lineNode *temp = head;  // Store the current node in a temporary variable.
-        head = head->next;       // Move to the next node.
-        free(temp->line);        // Free the line stored in the node.
-        free(temp);              // Free the node itself.
-    }
-}
--- src/rendering/movement.c
+++ src/rendering/movement.c
@@ -32,11 +32,15 @@ void move_left(t_game *game)
 {
     t_ray_coord *update;
 
+    // Get a pointer to the player's current position
     update = &game->player.pos;
+     // Calculate the new position based on moving left 90 degrees
     update->x += cos(game->player.rotAngle + angle_to_rad(-90)) * game->player.moveSpeed;
     update->y += sin(game->player.rotAngle + angle_to_rad(-90)) * game->player.moveSpeed;
+    // Check if the new position is valid within the map
     if(game->data->map_data.map[pixel_to_coord(update->y)][pixel_to_coord(update->x)] == '0')
     {
+        // If it's a valid move, update the player's position
         game->player.pos.x = update->x;
         game->player.pos.y = update->y;
     }
